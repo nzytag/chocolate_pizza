2773bcb95b4799233a17e039f59e1f4a448ad336
JEREMY PEARSON <JPJAZZY@HOTMAIL.COM>
2017-10-27 14:02:15 -0700

    added to js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
 'use strict';
+
+//get list items from list 1
+var list1item1 = document.getElementsByClassName('unchecked')[0];
+var list1item2 = document.getElementsByClassName('unchecked')[1];
+var list1item3 = document.getElementsByClassName('unchecked')[2];
+var list1item4 = document.getElementsByClassName('unchecked')[3];
+var list1item5 = document.getElementsByClassName('unchecked')[4];
+var list1item6 = document.getElementsByClassName('unchecked')[5];
+
+//get list items from list 2
+var list2item1 = document.getElementsByClassName('unchecked')[0];
+var list2item2 = document.getElementsByClassName('unchecked')[1];
+var list2item3 = document.getElementsByClassName('unchecked')[2];
+var list2item4 = document.getElementsByClassName('unchecked')[3];
+var list2item5 = document.getElementsByClassName('unchecked')[4];
+var list2item6 = document.getElementsByClassName('unchecked')[5];
+
+/*
+var picturesOfPats1of2 = document.getElementsByClassName('sub-banners')[0];
+var picturesOfPats2of2 = document.getElementsByClassName('sub-banners-2')[0];
+var upArrow = document.getElementsByClassName('up')[0];
+var downArrow = document.getElementsByClassName('down')[0];
+
+
+
+function togglePix() {
+
+  if (upArrow.getAttribute('class') === 'up inactivearrow') {
+    picturesOfPats1of2.setAttribute('class', 'sub-banners active');
+    picturesOfPats2of2.setAttribute('class', 'sub-banners-2 active');
+    upArrow.setAttribute('class', 'up activearrow');
+    downArrow.setAttribute('class', 'down inactivearrow');
+  } else {
+    picturesOfPats1of2.setAttribute('class', 'sub-banners inactive');
+    picturesOfPats2of2.setAttribute('class', 'sub-banners-2 inactive');
+    upArrow.setAttribute('class', 'up inactivearrow');
+    downArrow.setAttribute('class', 'down activearrow');
+  }
+}
+
+downArrow.addEventListener('click', togglePix);
+upArrow.addEventListener('click', togglePix);
+*/
