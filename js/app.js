77d59122c6a8b0a99c3666d4ff434e6749aef79b
JEREMY PEARSON <JPJAZZY@HOTMAIL.COM>
2017-10-27 15:27:56 -0700

    compiled code together for js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,32 @@
 'use strict';
 
 //get list items from list 1
-var list1item1 = document.getElementsByClassName('unchecked')[0];
-var list1item2 = document.getElementsByClassName('unchecked')[1];
-var list1item3 = document.getElementsByClassName('unchecked')[2];
-var list1item4 = document.getElementsByClassName('unchecked')[3];
-var list1item5 = document.getElementsByClassName('unchecked')[4];
-var list1item6 = document.getElementsByClassName('unchecked')[5];
+var list1item1 = document.getElementsByName('list1item1')[0];
+var list1item2 = document.getElementsByName('list1item2')[0];
+var list1item3 = document.getElementsByName('list1item3')[0];
+var list1item4 = document.getElementsByName('list1item4')[0];
+var list1item5 = document.getElementsByName('list1item5')[0];
+var list1item6 = document.getElementsByName('list1item6')[0];
 
 //get list items from list 2
-var list2item1 = document.getElementsByClassName('unchecked')[0];
-var list2item2 = document.getElementsByClassName('unchecked')[1];
-var list2item3 = document.getElementsByClassName('unchecked')[2];
-var list2item4 = document.getElementsByClassName('unchecked')[3];
-var list2item5 = document.getElementsByClassName('unchecked')[4];
-var list2item6 = document.getElementsByClassName('unchecked')[5];
+var list2item1 = document.getElementsByName('list2item1')[0];
+var list2item2 = document.getElementsByName('list2item2')[0];
+var list2item3 = document.getElementsByName('list2item3')[0];
+var list2item4 = document.getElementsByName('list2item4')[0];
+var list2item5 = document.getElementsByName('list2item5')[0];
+var list2item6 = document.getElementsByName('list2item6')[0];
 
-/*
-var picturesOfPats1of2 = document.getElementsByClassName('sub-banners')[0];
-var picturesOfPats2of2 = document.getElementsByClassName('sub-banners-2')[0];
-var upArrow = document.getElementsByClassName('up')[0];
-var downArrow = document.getElementsByClassName('down')[0];
+list1item1.addEventListener('click', toggleCheckbox);
+list1item2.addEventListener('click', toggleCheckbox);
+list1item3.addEventListener('click', toggleCheckbox);
+list1item4.addEventListener('click', toggleCheckbox);
+list1item5.addEventListener('click', toggleCheckbox);
+list1item6.addEventListener('click', toggleCheckbox);
 
-
-
-function togglePix() {
-
-  if (upArrow.getAttribute('class') === 'up inactivearrow') {
-    picturesOfPats1of2.setAttribute('class', 'sub-banners active');
-    picturesOfPats2of2.setAttribute('class', 'sub-banners-2 active');
-    upArrow.setAttribute('class', 'up activearrow');
-    downArrow.setAttribute('class', 'down inactivearrow');
+function toggleCheckbox(e) {
+  if (e.target.parentElement.getAttribute('class') === 'unchecked') {
+    e.target.parentElement.setAttribute('class', 'checked');
   } else {
-    picturesOfPats1of2.setAttribute('class', 'sub-banners inactive');
-    picturesOfPats2of2.setAttribute('class', 'sub-banners-2 inactive');
-    upArrow.setAttribute('class', 'up inactivearrow');
-    downArrow.setAttribute('class', 'down activearrow');
+    e.target.parentElement.setAttribute('class', 'unchecked');
   }
 }
-
-downArrow.addEventListener('click', togglePix);
-upArrow.addEventListener('click', togglePix);
-*/
